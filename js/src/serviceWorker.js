7bad05605630bf70ddbc5092362222d56e14f4a1
Jan Du <janxdu@gmail.com>
2020-05-12 16:26:22 +0800

    Site updated: 2020-05-12 16:26:13

---
+++ js/src/serviceWorker.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+  navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+
+})();
